cf9ebbee36656c95bbbb3e530bcd485bc89ab98d
Dominic.Klusek <Goldensonic96@yahoo.com>
2019-12-17 10:42:10 -0500

    Fixed Error in Parallel Markov Clustering
    Error was a result of the MPI_ANY_SOURCE in conjunction with each processor utilizing the same tag, thus when transferring data MPI took the first data point it received. Fixed by having each processor having their row/column number be their tag.

--- FinalProjectCode/Markov_Clustering.c
+++ FinalProjectCode/Markov_Clustering.c
@@ -63,14 +63,12 @@ int main(int argc, char *argv[]){
         FILE *infile;
         
         // open file containing graph data
-        infile = fopen("graph.txt","r");
+        infile = fopen("graph2.txt","r");
         
         while(fscanf(infile,"%d %d %f",&node_1, &node_2, &weight) != 0){
             //printf("Temp val: %f\n", temp);
             data[node_1 * sqrt_p + node_2] = weight;
             data[node_2 * sqrt_p + node_1] = weight;
-            //printf("Data[%i] value: %f\n", node_1 * sqrt_p + node_2, data[node_1 * sqrt_p + node_2]);
-            //printf("Data[%i] value: %f\n", node_2 * sqrt_p + node_1, data[node_2 * sqrt_p + node_1]);
         }
         
         // add self loops
@@ -84,15 +82,20 @@ int main(int argc, char *argv[]){
     }
     
     if(world_rank == 0){
-        for(i = 0; i < world_size; i++){
-            printf("Data[%i] value: %f\n", i, data[i]);
-        }
+        printf("Original Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
     }
     
     // Scatter data to other processors, and copy data into second value
     MPI_Scatter(data, 1, MPI_FLOAT, &matrix_val, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
     
-    printf("Processor %i: %f\n", world_rank, matrix_val);
+    //printf("Processor %i: %f\n", world_rank, matrix_val);
     
     //free up dynamic memory
     if(world_rank == 0){
@@ -126,7 +129,6 @@ int main(int argc, char *argv[]){
  
     // create column indices for group
     for(i = 0; i < sqrt_p; i++){
-        //printf("%i: %i\n", i, start + i * sqrt_p);
         column_indices[i] = start + i * sqrt_p;
     }
  
@@ -140,55 +142,121 @@ int main(int argc, char *argv[]){
  
     // Create a new communicator based on the group
     MPI_Comm_create_group(MPI_COMM_WORLD, prime_group, 0, &column_comm);
-   
     
-    MPI_Comm_size(column_comm, &size);
-    //printf("Size of column: %i\n", size);
-    
-    MPI_Comm_rank(row_comm, &size);
-    //printf("World rank: %i\tRow Rank: %i\n", world_rank, size);
-    
-    MPI_Comm_rank(column_comm, &size);
-    //printf("World rank: %i\tColumn Rank: %i\n", world_rank, size);
-    
-    //printf("World rank: %i\tRow Number: %i\n", world_rank, row_number);
+    //printf("World rank: %i Row Number: %i\n", world_rank, row_number);
     //printf("World rank: %i\tColumn Number: %i\n", world_rank, column_number);
     
     // normalize matrix values before aligning
     MPI_Allreduce(&matrix_val, &column_sum, 1, MPI_FLOAT, MPI_SUM, column_comm);
-    matrix_val /= column_sum;
-
+    matrix_val = matrix_val / column_sum;
+    
+//     printf("World rank: %i Column Sum: %f\n", world_rank, column_sum);
+    
+    // copy matrix val to second matrix variable
     matrix_val_2 = matrix_val;
+    
+    MPI_Gather(&matrix_val, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Matrix 1\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
+        
+    MPI_Gather(&matrix_val_2, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Matrix 2\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
+    
+    //printf("World rank: %i Matrix Val: %f\n", world_rank, matrix_val);
+    //printf("World rank: %i Matrix Val 2: %f\n", world_rank, matrix_val_2);
+    
+    MPI_Gather(&matrix_val, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Markov Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
     
     // get row and column ranks
     int row_rank, column_rank;
     MPI_Comm_rank(row_comm, &row_rank);
     MPI_Comm_rank(column_comm, &column_rank);
+//     printf("Processor %d Column Rank %d\n", world_rank, column_rank);
     
     // align rows and columns
     MPI_Request req;
     
-    MPI_Isend(&matrix_val, 1, MPI_FLOAT, ((row_rank - row_number) + sqrt_p) % sqrt_p, 0, row_comm, &req);
-    MPI_Recv(&matrix_val, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, row_comm, MPI_STATUS_IGNORE);
+    
+//     printf("Row align Processor %d to Processor: %d\n", world_rank, ((row_rank - row_number) + sqrt_p) % sqrt_p);
+    if(row_number != 0){
+        //matrix_val = row_rank;
+        MPI_Isend(&matrix_val, 1, MPI_FLOAT, ((row_rank - row_number) + sqrt_p) % sqrt_p, row_number, row_comm, &req);
+        MPI_Recv(&matrix_val, 1, MPI_FLOAT, MPI_ANY_SOURCE, row_number, row_comm, MPI_STATUS_IGNORE);
 
-    MPI_Wait(&req, MPI_STATUS_IGNORE);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+    }
     
-    MPI_Isend(&matrix_val_2, 1, MPI_FLOAT, ((column_rank - column_number) + sqrt_p) % sqrt_p, 1, column_comm, &req);
-    MPI_Recv(&matrix_val_2, 1, MPI_FLOAT, MPI_ANY_SOURCE, 1, column_comm, MPI_STATUS_IGNORE);
-    MPI_Wait(&req, MPI_STATUS_IGNORE);
+    if(column_number != 0){
+        //matrix_val_2 = column_rank;
+        MPI_Isend(&matrix_val_2, 1, MPI_FLOAT, ((column_rank - column_number) + sqrt_p) % sqrt_p, column_number, column_comm, &req);
+        MPI_Recv(&matrix_val_2, 1, MPI_FLOAT, MPI_ANY_SOURCE, column_number, column_comm, MPI_STATUS_IGNORE);
+        
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+    }
     
-    //printf("Processor %i Initial Matrix Value 1: %f\n", world_rank, matrix_val);
-    //printf("Processor %i Initial Matrix Value 2: %f\n", world_rank, matrix_val_2);
+    MPI_Gather(&matrix_val, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Aligned Row Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
+    
+    MPI_Gather(&matrix_val_2, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Aligned Column Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
     
     // every processor performs these actions
-    while(difference >= 0.1){
-        printf("Look\n");
+    while(difference >= 0.000001){
         // reset column sum, and expanded val
         column_sum = 0;
         expanded_val = 0;
     
         // perform expansion operations
         for(i = 0; i < sqrt_p; i++){
+            if(world_rank == 0){
+                //printf("Moving Values\n");
+            }
+            
             // non blocking send to the next row (cicular array)
             MPI_Isend(&matrix_val_2, 1, MPI_FLOAT, (column_rank - 1 + sqrt_p) % sqrt_p, 1, column_comm, &req);
             MPI_Recv(&matrix_val_2, 1, MPI_FLOAT, MPI_ANY_SOURCE, 1, column_comm, MPI_STATUS_IGNORE);
@@ -202,19 +270,32 @@ int main(int argc, char *argv[]){
             // sum up product of matrix values
             expanded_val += (matrix_val * matrix_val_2);
         }
+        
+        MPI_Gather(&expanded_val, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        /*if(world_rank == 0){
+            printf("Expanded Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }*/
+        
     
         // perform first inflation step
-        inflated_val = pow(expanded, inflation_factor);
-        //printf("%f ^ %f %f\n",matrix_val, inflation_factor, inflated_val);
+        inflated_val = pow(expanded_val, inflation_factor);
         
         // get sum of column, and store result in column_sum of each processor in column
         MPI_Allreduce(&inflated_val, &column_sum, 1, MPI_FLOAT, MPI_SUM, column_comm);
+//         printf("%f ^ %f %f\n", expanded_val, inflation_factor, inflated_val);
         
         // normalize inflated matrix values
         inflated_val /= column_sum;
     
         // calculate difference between expanded matrix, and inflated matrix
-        difference = abs(expanded_val - inflated_val);
+        difference = fabs(expanded_val - inflated_val);
         
         //store new values 
         matrix_val = inflated_val;
@@ -222,10 +303,35 @@ int main(int argc, char *argv[]){
     
         // get sum the total difference between expanded and inflated values
         MPI_Allreduce(&difference, &difference, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+        
+        
+        /*MPI_Gather(&matrix_val, 1, MPI_FLOAT, data, world_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Next iteration\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }*/
     }
     
-    printf("Processor %i Final Matrix Value 1: %f\n", world_rank, matrix_val);
-    printf("Processor %i Final Matrix Value 2: %f\n", world_rank, matrix_val_2);
+    MPI_Gather(&matrix_val, 1, MPI_FLOAT, data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        if(world_rank == 0){
+            printf("Final Matrix\n");
+            for(i = 0; i < sqrt_p; i++){
+                for(j = 0; j < sqrt_p; j++){
+                    //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                    printf("%f ", data[sqrt_p * i + j]);
+                }
+            printf("\n");
+            }
+        }
+    
+    //printf("Processor %i Final Matrix Value 1: %f\n", world_rank, matrix_val);
+    //printf("Processor %i Final Matrix Value 2: %f\n", world_rank, matrix_val_2);
     //printf("Processor %i Final Matrix Value: %i\n", world_rank, matrix_val);
     MPI_Finalize(); // close MPI environment
 }
---
+++ FinalProjectCode/Markov_Clustering_Sequential.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <mpi.h>
+#include <math.h>
+#include <time.h>
+//mpirun -np 8 --use-hwthread-cpus ./parrallelSum
+
+int main(int argc, char *argv[]){
+    // loop counter variables
+    int i, j, z, number_of_nodes;
+    
+    // variable to hold difference between expanded and inflated matrix values
+    float difference = 1;
+    
+    // matrix values that are held by processor
+    float matrix_val;
+    float matrix_val_2;
+    
+    // variables to hold the calculated expanded and inflated values
+    float **expanded_val;
+    float **inflated_val;
+    
+    // variable to hold very temporary values
+    float temp;
+    
+    // variable to hold the sum of the column
+    float column_sum;
+ 
+    // inflation factor, which can be changed to any float value > 1
+    float inflation_factor = 2.0;
+    
+    // data array to hold intiial markov matrix (only processor 0 uses this)
+    float **data;
+
+    
+    // processor 0 must get data and add self loops, needs to be 1-D for easy data transfer to
+    // other processors
+    int node_1, node_2;
+    float weight;
+        
+    // file handler
+    FILE *infile;
+        
+    // open file containing graph data
+    infile = fopen("graph_sequentail.txt","r");
+    
+    fscanf(infile, "%d", &number_of_nodes);
+    
+    // allocate data array to hold node connection data
+    data = (float**)malloc(number_of_nodes * sizeof(float *));
+    
+    for(i = 0; i < number_of_nodes; i++){
+        data[i] = (float *)malloc(number_of_nodes * sizeof(float));
+    }
+    
+    // get node weights
+    while(fscanf(infile,"%d %d %f",&node_1, &node_2, &weight) != 0){
+        data[node_1][node_2] = weight;
+        data[node_2][node_1] = weight;
+    }
+        
+    // add self loops
+    for(i = 0; i < number_of_nodes; i++){
+        data[i][i] = 1.0;
+    }
+        
+    // close file
+    fclose(infile);
+    printf("Closed the file\n");
+        
+    for(i = 0; i < number_of_nodes; i++){
+        for(j = 0; j < number_of_nodes; j++){
+            //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+            printf("%f ", data[i][j]);
+        }
+        printf("\n");
+    }
+
+    // create Markov Matrix
+    for(i = 0; i < number_of_nodes; i++){
+        column_sum = 0;
+        for(j = 0; j < number_of_nodes; j++){
+            column_sum += data[j][i];
+        }
+        for(j = 0; j < number_of_nodes; j++){
+            data[j][i] /= column_sum;
+        }
+    }
+    
+    printf("Original Markov Matrix\n");
+    for(i = 0; i < number_of_nodes; i++){
+        for(j = 0; j < number_of_nodes; j++){
+            //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+            printf("%f ", data[i][j]);
+        }
+        printf("\n");
+    }
+    
+    // allocate data array to hold node connection data
+    expanded_val = (float**)malloc(number_of_nodes * sizeof(float *));
+    
+    for(i = 0; i < number_of_nodes; i++){
+        expanded_val[i] = (float *)malloc(number_of_nodes * sizeof(float));
+    }
+    
+    // allocate data array to hold node connection data
+    inflated_val = (float**)malloc(number_of_nodes * sizeof(float *));
+    
+    for(i = 0; i < number_of_nodes; i++){
+        inflated_val[i] = (float *)malloc(number_of_nodes * sizeof(float));
+    }
+    printf("Dynamically alocatted all memory\n");
+    
+    // every processor performs these actions
+    while(difference >= 0.0000001){
+        // perform expansion and first step of inflation operations in same loop
+        for(i = 0; i < number_of_nodes; i++){
+            for(j = 0; j < number_of_nodes; j++){
+                expanded_val[i][j] = 0.0;
+                for(z = 0; z < number_of_nodes; z++){
+                    expanded_val[i][j] += (data[i][z] * data[z][j]);
+                }
+                inflated_val[i][j] = pow(expanded_val[i][j], inflation_factor);
+            }
+        }
+        
+        printf("Expanded Matrix\n");
+        for(i = 0; i < number_of_nodes; i++){
+            for(j = 0; j < number_of_nodes; j++){
+                //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                printf("%f ", expanded_val[i][j]);
+            }
+            printf("\n");
+        }
+        
+        //perform second step of inflation operation
+        for(i = 0; i < number_of_nodes; i++){
+            column_sum = 0;
+            for(j = 0; j < number_of_nodes; j++){
+                column_sum += inflated_val[j][i];
+            }
+            for(j = 0; j < number_of_nodes; j++){
+                inflated_val[j][i] /= column_sum;
+            }
+        }
+        
+        // reset difference variable
+        difference = 0;
+        
+        // find difference and replace data values with inflated values
+        for(i = 0; i < number_of_nodes; i++){
+            for(j = 0; j < number_of_nodes; j++){
+                difference += fabs((expanded_val[i][j] - inflated_val[i][j]));
+                data[i][j] = inflated_val[i][j];
+            }
+        }
+        
+        printf("Next iteration\n");
+        for(i = 0; i < number_of_nodes; i++){
+            for(j = 0; j < number_of_nodes; j++){
+                //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+                printf("%f ", data[i][j]);
+            }
+        printf("\n");
+        }
+        
+        printf("Total Difference: %f\n", difference);
+    }
+    
+    printf("Final Array\n");
+    for(i = 0; i < number_of_nodes; i++){
+        for(j = 0; j < number_of_nodes; j++){
+            //printf("Final Data[%i][%i] value: %f\n", i, j, data[i][j]);
+            printf("%f ", data[i][j]);
+        }
+        printf("\n");
+    }
+    
+
+    for(i = 0; i < number_of_nodes; i++){
+        free(inflated_val[i]);
+        free(expanded_val[i]);
+        free(data[i]);
+    }
+    
+    free(inflated_val);
+    free(expanded_val);
+    free(data);
+}
